3c312dadbd96adb065c84e3c90afa3f709452e5b
Curtis Badke <curtis@badkid.ca>
2016-11-13 17:40:24 -0700

    more appveyor fixing

--- automatic/jenkins/update.ps1
+++ automatic/jenkins/update.ps1
@@ -1,6 +1,7 @@
 import-module au
 
 $url = 'http://mirrors.jenkins-ci.org/windows-stable/latest'
+$localPath = $PSScriptRoot
 
 function global:au_SearchReplace {
     @{
@@ -13,20 +14,13 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 
-    $location = ./curl.exe -sI $url | Where-Object {$_ -like "Location: *"}
-
-    Write-Host = $location
+    $location = (iex "$localPath/curl.exe -sI '$url'") | Where-Object {$_ -like "Location: *"}
 
     $filename = $location.Substring($location.LastIndexOf("/") + 1)
     $version = ($filename -split '-|\.' | select -Last 3 -skip 1) -join '.'
 
     $checkSumUrl = "http://mirrors.jenkins-ci.org/windows-stable/$filename.sha256"
-    $checksum = (.\curl.exe $checkSumUrl).Split(' ')[0]
-
-
-    Write-Host $version
-    Write-Host $checkSum
-
+    $checksum = (iex "$localPath/curl.exe '$checkSumUrl'").Split(' ')[0]
 
     $Latest = @{ URL = $location; Version = $version; CheckSum32 = $checkSum; CheckSumType = 'sha256' }
     return $Latest
